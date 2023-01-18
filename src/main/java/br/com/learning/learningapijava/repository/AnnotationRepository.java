d0c922b1ed8d174da8d9cd97f704f344c83a1cd7
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-18 10:32:28 -0300

    🚧 feat: criou o repositório de anotação

---
+++ src/main/java/br/com/learning/learningapijava/repository/AnnotationRepository.java
@@ -0,0 +1,62 @@
+package br.com.learning.learningapijava.repository;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.learning.learningapijava.model.AnnotationUser;
+
+@Repository
+public class AnnotationRepository {
+
+    List<AnnotationUser> annotationBd = new ArrayList<AnnotationUser>(); 
+    Integer annotationUserId = 0;
+
+
+    public List<AnnotationUser> getAllAnnotations() {
+
+        return annotationBd;
+    }
+
+
+    public Optional<AnnotationUser> getAnnotationById(Integer id) {
+
+       return annotationBd.stream().filter(annotation -> annotation.getId() == id).findFirst();
+
+    }
+
+    public AnnotationUser registerAnnotation(AnnotationUser annotation) {
+
+        annotationUserId++;
+        // mudar para o id do usuario
+
+        annotation.setId(annotationUserId);
+
+        annotationBd.add(annotation);
+
+        return annotation;
+    }
+
+    private AnnotationUser updateAnnotation(AnnotationUser annotation) {
+
+        Optional<AnnotationUser> haveUser = getAnnotationById(annotation.getId());
+
+        if (haveUser.isEmpty()) {
+
+            throw new InputMismatchException("Usuário não encontrado.");
+        }
+
+        deleteAnnotation(annotation.getId());
+
+        annotationBd.add(annotation);
+        return annotation;
+    }
+
+    public void deleteAnnotation(Integer id) {
+
+        annotationBd.removeIf(annotation -> annotation.getId() == id);
+    }
+}
