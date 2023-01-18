436f490af8cf30c1f57f611777c3ff1d4285cf39
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-18 10:09:47 -0300

    🚧 feat: criou o model da anotação do usuário

---
+++ src/main/java/br/com/learning/learningapijava/model/AnnotationUser.java
@@ -0,0 +1,56 @@
+package br.com.learning.learningapijava.model;
+
+public class  AnnotationUser {
+    
+    private Integer id;
+
+    private String title;
+
+    private String date;
+
+    private String description;
+
+    private String text;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+   
+}
