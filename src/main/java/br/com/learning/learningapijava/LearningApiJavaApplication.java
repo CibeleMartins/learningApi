8071d782f66aafd536230dd433b39975351ef975
CibeleMartins <cibelemartins24@hotmail.com>
2023-01-18 09:55:45 -0300

    🎉 Iniciou o repositório

---
+++ src/main/java/br/com/learning/learningapijava/LearningApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.learning.learningapijava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/learning/learningapijava/LearningApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.learning.learningapijava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
